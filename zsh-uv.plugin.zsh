e371bb2976a45a29b161536547fe55492510127d
Tomáš Lipovský <tomas@lipovo.cz>
2024-11-24 00:22:22 +0000

    Fix autocompletion

--- zsh-uv.plugin.zsh
+++ zsh-uv.plugin.zsh
@@ -1,16 +1,25 @@
 #!/usr/bin/env zsh
+# shellcheck disable=SC1090
 
-if (( ! $+commands[uv] )); then
-  return
+# Exit if the 'fnm' command can not be found
+if ! (( $+commands[uv] )); then
+    return
 fi
 
+# Completions directory for `uv` command
 local COMPLETIONS_DIR="${0:A:h}/completions"
-
-# Only regenerate completions if does not exist or older than 24 hours
-if [[ ! -f "$COMPLETIONS_DIR/_uv" || ! $(find "$COMPLETIONS_DIR/_uv" -newermt "24 hours ago" -print) ]]; then
-  uv generate-shell-completion zsh 2> /dev/null >| "$COMPLETIONS_DIR/_uv" &|
-fi
 
 # Add completions to the FPATH
 typeset -TUx FPATH fpath
 fpath=("$COMPLETIONS_DIR" $fpath)
+
+# If the completion file does not exist yet, then we need to autoload
+# and bind it to `uv`. Otherwise, compinit will have already done that.
+if [[ ! -f "$COMPLETIONS_DIR/_uv" ]]; then
+    typeset -g -A _comps
+    autoload -Uz _uv
+    _comps[uv]=_uv
+fi
+
+# Generate and load completion in the background
+uv generate-shell-completion zsh 2> /dev/null >| "$COMPLETIONS_DIR/_uv" &|
